5fb0ad0a668f0557ef5b47f99419d996b91f8591
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-02 07:00:10 +0545

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the noteAppApp
+ */
+angular.module('noteAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noteAppApp
+ */
+angular.module('noteAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
